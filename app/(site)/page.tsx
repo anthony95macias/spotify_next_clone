4e2db0a18fa4496757a2e01d8ffe25a60158c5d6
Anthony Macias <Anthony40macias@gmail.com>
2023-06-30 09:27:21 -0700

    made changes from boiler plate next code to personal config.

---
+++ app/(site)/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className='text-green-500'>
+        Hello Spotify Again!!
+      </h1> 
+    </div>
+  );
+}; 
+
+export default Page;
+
