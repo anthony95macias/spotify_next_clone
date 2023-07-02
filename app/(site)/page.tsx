23a23e7cf272102093dce832cd8cb2a073aae383
Anthony Macias <Anthony40macias@gmail.com>
2023-07-02 00:13:34 -0700

    created a sidebar with compoenets-Home/search/ Song library

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-green-500'>
-        Hello Spotify Again!!
-      </h1> 
+    <div className='text-green-500'>
+        Main Content 
     </div>
   );
 }; 
