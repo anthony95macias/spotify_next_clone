2030b208a6e4f992166f3e2bbb14b12545631370
Anthony Macias <Anthony40macias@gmail.com>
2023-07-09 23:51:51 -0700

    added some components. Foward/Back button. Mobile a Home/Search button. Also added a Sign up button.

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,9 +1,18 @@
+import Header from '@/components/Header';
 import React from 'react';
 
 const Page = () => {
   return (
-    <div className='text-green-500'>
-        Main Content 
+    <div className='
+      bg-neutral-900
+        rounded-lg
+        h-full
+        w-full
+        
+    '> 
+    <Header>
+      Header
+    </Header>
     </div>
   );
 }; 
